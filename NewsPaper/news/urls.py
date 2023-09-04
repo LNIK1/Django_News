3d18d14eb44680aa61f4c2fcb5c8d2b2ebba087d
LNIK1 <lapchenko27@gmail.com>
2023-09-04 10:28:41 +0700

    add News & News/search pages

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete, WrongTypeUpdateException
+from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete, NewsList, NewsSearch, ArticlesList, WrongTypeUpdateException
 
 urlpatterns = [
     path('', PostList.as_view(), name='posts'),
     path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('news/', NewsList.as_view(), name='news'),
+    path('news/search/', NewsSearch.as_view(), name='news_search'),
     path('news/create/', PostCreate.as_view(), name='news_create'),
     path('news/<int:pk>/update', PostUpdate.as_view(), name='news_update'),
     path('news/<int:pk>/delete', PostDelete.as_view(), name='news_delete'),
+    path('articles/', ArticlesList.as_view(), name='articles'),
     path('articles/create/', PostCreate.as_view(), name='articles_create'),
     path('articles/<int:pk>/update', PostUpdate.as_view(), name='articles_update'),
     path('articles/<int:pk>/delete', PostDelete.as_view(), name='articles_delete'),
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -11,23 +11,9 @@ from datetime import datetime
 class PostList(ListView):
 
     model = Post
-    # ordering = 'title'
     queryset = Post.objects.order_by('-post_date')
     template_name = 'posts.html'
     context_object_name = 'posts'
-    paginate_by = 10
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        self.filterset = PostFilter(self.request.GET, queryset)
-
-        return self.filterset.qs
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)  # вернет словарь
-        context['filterset'] = self.filterset
-
-        return context
 
 
 class PostDetail(DetailView):
@@ -42,7 +28,7 @@ class PostCreate(CreateView):
     form_class = PostForm
     model = Post
     template_name = 'post_create.html'
-    success_url = reverse_lazy('<int:pk>/post_detail')
+    success_url = reverse_lazy('posts')
 
     def form_valid(self, form):
         post = form.save(commit=False)
@@ -80,6 +66,45 @@ class PostDelete(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('posts')
+
+
+class NewsList(ListView):
+
+    model = Post
+    queryset = Post.objects.filter(p_type='NE').order_by('-post_date')
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+
+class NewsSearch(ListView):
+
+    model = Post
+    queryset = Post.objects.filter(p_type='NE').order_by('-post_date')
+    template_name = 'news_search.html'
+    context_object_name = 'news'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  # вернет словарь
+        context['filterset'] = self.filterset
+
+        return context
+
+
+class ArticlesList(ListView):
+
+    model = Post
+    queryset = Post.objects.filter(p_type='AR').order_by('-post_date')
+    template_name = 'articles.html'
+    context_object_name = 'articles'
+    paginate_by = 10
 
 
 class WrongTypeUpdateException(ListView):
